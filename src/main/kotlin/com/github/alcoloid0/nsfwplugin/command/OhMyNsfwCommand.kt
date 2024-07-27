53204a1c630b0fbc08fd3b36ec9e696097b60adb
alcoloid0 <171401998+alcoloid0@users.noreply.github.com>
2024-07-27 22:10:53 +0300

    feat: add multiple Reddit subcommands

--- src/main/kotlin/com/github/alcoloid0/nsfwplugin/command/OhMyNsfwCommand.kt
+++ src/main/kotlin/com/github/alcoloid0/nsfwplugin/command/OhMyNsfwCommand.kt
@@ -21,7 +21,9 @@ import com.github.alcoloid0.nsfwplugin.OhMyNsfwPlugin
 import com.github.alcoloid0.nsfwplugin.extra.NekoBotImageType
 import com.github.alcoloid0.nsfwplugin.extra.sendSettingsMessage
 import com.github.alcoloid0.nsfwplugin.map.ImageMap
-import com.github.alcoloid0.nsfwplugin.provider.ImageProvider
+import com.github.alcoloid0.nsfwplugin.provider.impl.NekoBotImageProvider
+import com.github.alcoloid0.nsfwplugin.provider.impl.RedditImageProvider
+import com.github.alcoloid0.nsfwplugin.provider.impl.Rule34ImageProvider
 import org.bukkit.entity.Player
 import revxrsal.commands.annotation.Command
 import revxrsal.commands.annotation.Optional
@@ -31,11 +33,13 @@ import revxrsal.commands.bukkit.annotation.CommandPermission
 
 @Command("ohmynsfw", "nsfw")
 class OhMyNsfwCommand {
+    // TODO: provider router/manager/service
+
     @Subcommand("nekobot")
     @CommandPermission("ohmynsfw.use.nekobot")
     fun onNekoBot(player: Player, imageType: NekoBotImageType) {
         ImageMap.request(player) {
-            ImageProvider.NekoBot.getRandomImage(imageType.toString())
+            NekoBotImageProvider().getRandomImage(imageType.toString())
         }
     }
 
@@ -43,7 +47,89 @@ class OhMyNsfwCommand {
     @CommandPermission("ohmynsfw.use.rule34")
     fun onRule34(player: Player, @Optional tags: String = "") {
         ImageMap.request(player) {
-            ImageProvider.Rule34.getRandomImage(*tags.split(" ").toTypedArray())
+            Rule34ImageProvider().getRandomImage(*tags.split(" ").toTypedArray())
+        }
+    }
+
+    // TODO: Refactor this
+
+    @Subcommand("r/nsfw")
+    @CommandPermission("ohmynsfw.use.reddit.nsfw")
+    fun onRedditNsfw(player: Player) {
+        ImageMap.request(player) {
+            RedditImageProvider("nsfw").getRandomImage()
+        }
+    }
+
+    @Subcommand("r/nsfw2")
+    @CommandPermission("ohmynsfw.use.reddit.nsfw2")
+    fun onRedditNsfw2(player: Player) {
+        ImageMap.request(player) {
+            RedditImageProvider("nsfw2").getRandomImage()
+        }
+    }
+
+    @Subcommand("r/hentai")
+    @CommandPermission("ohmynsfw.use.reddit.hentai")
+    fun onRedditHentai(player: Player) {
+        ImageMap.request(player) {
+            RedditImageProvider("hentai").getRandomImage()
+        }
+    }
+
+    @Subcommand("r/bdsm")
+    @CommandPermission("ohmynsfw.use.reddit.bdsm")
+    fun onRedditBdsm(player: Player) {
+        ImageMap.request(player) {
+            RedditImageProvider("bdsm").getRandomImage()
+        }
+    }
+
+    @Subcommand("r/anal")
+    @CommandPermission("ohmynsfw.use.reddit.anal")
+    fun onRedditAnal(player: Player) {
+        ImageMap.request(player) {
+            RedditImageProvider("anal").getRandomImage()
+        }
+    }
+
+    @Subcommand("r/boobs")
+    @CommandPermission("ohmynsfw.use.reddit.boobs")
+    fun onRedditBoobs(player: Player) {
+        ImageMap.request(player) {
+            RedditImageProvider("boobs").getRandomImage()
+        }
+    }
+
+    @Subcommand("r/legalteens")
+    @CommandPermission("ohmynsfw.use.reddit.legalteens")
+    fun onRedditLegalTeens(player: Player) {
+        ImageMap.request(player) {
+            RedditImageProvider("LegalTeens").getRandomImage()
+        }
+    }
+
+    @Subcommand("r/furry")
+    @CommandPermission("ohmynsfw.use.reddit.furry")
+    fun onRedditFurry(player: Player) {
+        ImageMap.request(player) {
+            RedditImageProvider("furry").getRandomImage()
+        }
+    }
+
+    @Subcommand("r/toocuteforporn")
+    @CommandPermission("ohmynsfw.use.reddit.toocuteforporn")
+    fun onRedditTooCuteForPorn(player: Player) {
+        ImageMap.request(player) {
+            RedditImageProvider("TooCuteForPorn").getRandomImage()
+        }
+    }
+
+    @Subcommand("r/just18")
+    @CommandPermission("ohmynsfw.use.reddit.just18")
+    fun onRedditJust18(player: Player) {
+        ImageMap.request(player) {
+            RedditImageProvider("Just18").getRandomImage()
         }
     }
 
